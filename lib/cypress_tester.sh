43f37419643be1de83a1da5fdfe65964cbc9959a
Dmitry Savchenko <dg.freak@gmail.com>
2019-08-23 14:18:07 +0300

    UUID instead of UID

--- lib/cypress_tester.sh
+++ lib/cypress_tester.sh
@@ -5,27 +5,27 @@ argv=($@)
 
 SOURCE_REP=$1
 
-UID="cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32"
+UUID="$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 6)"
 
 PORT="$(bash ../../lib/port.sh)"
 
 mkdir reps
 cd reps
-git clone $SOURCE_REP origin$UID
-mkdir origin$UID/cypress
-mkdir origin$UID/cypress/integration
+git clone $SOURCE_REP origin$UUID
+mkdir origin$UUID/cypress
+mkdir origin$UUID/cypress/integration
 for (( j=1; j<argc; j++ )); do
-  git clone ${argv[j]} tests$j$UID
-  yes | cp -rf ./tests$j$UID/cypress/integration/* ./origin$UID/cypress/integration/
+  git clone ${argv[j]} tests$j$UUID
+  yes | cp -rf ./tests$j$UUID/cypress/integration/* ./origin$UUID/cypress/integration/
 done
-cd origin$UID
+cd origin$UUID
 mkdir docker
 cp ../../lib/Dockerfile docker/
 cp ../../lib/start2.sh docker/
 
 echo "{\"baseUrl\": \"http://localhost:$PORT/\"}" > cypress.json
 docker build -t $PORT -f docker/Dockerfile .
-DOCKER_PID="$(docker run  -d -p $PORT:80 --name $PORT --rm $PORT)"
+DOCKER_PID="$(docker run -d -p $PORT:80 --name $PORT --rm $PORT)"
 
 npm install --save-dev cypress mocha mocha-spec-json-reporter
 npx cypress run --reporter mocha-spec-json-reporter > ../../log/output.log
@@ -35,5 +35,5 @@ mv mocha-output.json ../../log/
 docker rmi --force $(docker images -q $SOURCE_REP | uniq)
 
 cd ..
-rm -rf ./origin$UID
-rm -rf ./tests*$UID
+rm -rf ./origin$UUID
+rm -rf ./tests*$UUID
